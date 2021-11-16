3a92d173a82f6f03a4e667132309b80215c571b1
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-16 18:39:17 +0000

    Header component created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import './App.css';
 import icon from './cat.png'
 import Navbar from './components/Navbar.js'
+import Header from './components/Header.js';
 
 function App() {
   return (
     <>
       <Navbar />
 
-      <header className="header__container">
-        <h1 className="header__title header__welcome">Welcome to React!</h1>
-        <h2 className="header__title">There are currently 9 Cats in this Cat App</h2>
-      </header>
+      <Header />
 
       <main>
         <div className="cards__wrapper">
