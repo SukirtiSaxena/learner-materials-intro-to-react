1d8988edf9509f9bd37ecdead5e4d99abd7b1c0e
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-17 19:39:38 +0000

    Refactored Navbar by using map

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import ListOfLinks from './ListOfLinks';
 function Navbar() {
+    const listItems = [
+          { name: "Home", link:"home" },
+          { name: "Mews", link:"mews" },
+          { name: "Contact", link:"contact" },
+          { name: "About", link:"https://www.cats.org.uk/sponsor" }
+    ];
     return (
         <nav className="nav__container">
             <ul className="nav__container ul ul__navbar">
-             <ListOfLinks name="Home" link="home"/>
-             <ListOfLinks name="Mews" link="mews"/>
-             <ListOfLinks name="Contact" link="contact"/>
-             <ListOfLinks name="About" link="https://www.cats.org.uk/sponsor"/>
-            </ul>
+            {listItems.map((listItem, i) => 
+            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
+           </ul>
         </nav>
     )
 }
