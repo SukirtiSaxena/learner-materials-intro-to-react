3a7f70b512bc91e6a41d4077cbf623a10c54db8a
SukirtiSaxena <sukirtisaxena@gmail.com>
2021-11-17 19:54:31 +0000

    re-used component ListOfLinks in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,49 @@
 import icon from '../cat.png'
+import ListOfLinks from './ListOfLinks';
 function Footer() {
+  const list1Items = [
+    { name: "About", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Belly Rubs", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Donate", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Cat Nip", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Customers", link: "https://www.cats.org.uk/sponsor" }
+  ];
+  const list2Items = [
+    { name: "Resources", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Dogs", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Blog", link: "https://www.cats.org.uk/sponsor" },
+    { name: "eBooks", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Revenge", link: "https://www.cats.org.uk/sponsor" }
+  ];
+  const list3Items = [
+    { name: "Contact", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Help", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Blog", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Meow", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Mice", link: "https://www.cats.org.uk/sponsor" }
+  ];
+  const list4Items = [
+    { name: "Ts & Cs", link: "https://www.cats.org.uk/sponsor" },
+    { name: "Privacy Policy", link: "https://www.cats.org.uk/sponsor" }
+  ];
   return (
     <footer>
       <section className="nav__container footer">
         <ul className="footer__section ul">
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">About</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Belly Rubs</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Donate</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Cat Nip</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Customers</a></li>
+          {list1Items.map((listItem, i) =>
+            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
         </ul>
         <ul className="footer__section ul">
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Resources</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Dogs</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Blog</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">eBooks</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Revenge</a></li>
+          {list2Items.map((listItem, i) =>
+            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
         </ul>
         <ul className="footer__section ul">
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Contact</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Help</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Meow</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Mice</a></li>
+          {list3Items.map((listItem, i) =>
+            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
         </ul>
         <ul className="footer__section ul">
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Ts &amp; Cs</a></li>
-          <li><a className="links" href="https://www.cats.org.uk/sponsor">Privacy Policy</a></li>
+          {list4Items.map((listItem, i) =>
+            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
           <li className="links">&copy; 2021 TR Inc.</li>
           <i><img className="icon" src={icon} alt="A ginger cat silhouette" /></i>
         </ul>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import ListOfLinks from './ListOfLinks';
 function Navbar() {
     const listItems = [
-          { name: "Home", link:"home" },
-          { name: "Mews", link:"mews" },
-          { name: "Contact", link:"contact" },
-          { name: "About", link:"https://www.cats.org.uk/sponsor" }
+        { name: "Home", link: "home" },
+        { name: "Mews", link: "mews" },
+        { name: "Contact", link: "contact" },
+        { name: "About", link: "https://www.cats.org.uk/sponsor" }
     ];
     return (
         <nav className="nav__container">
             <ul className="nav__container ul ul__navbar">
-            {listItems.map((listItem, i) => 
-            <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
-           </ul>
+                {listItems.map((listItem, i) =>
+                    <ListOfLinks name={listItem.name} link={listItem.link} key={i} />)}
+            </ul>
         </nav>
     )
 }
